0176555ee3a0159d17f81c484683837084a9a13c
sean00880 <theelitereapersanctum@gmail.com>
2022-09-02 03:00:50 -0400

    next new354

--- components/Intro.js
+++ components/Intro.js
@@ -28,7 +28,7 @@ const Intro = () => {
           
           <Image
                 className={style.image}
-                src="../public/img/GrandpaRon.png"
+                src="/img/GrandpaRon.png"
                 width="500"
                 height="500"
                 priority={true}
